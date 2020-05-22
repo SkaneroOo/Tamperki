ed5555eace5eadcb484af5a6b53d4c68aa40e02c
SkaneroOo <40175161+SkaneroOo@users.noreply.github.com>
2020-05-22 09:59:52 +0200

    Update antytestportal.js

--- antytestportal.js
+++ antytestportal.js
@@ -3,19 +3,14 @@
 // @include  https://www.testportal.pl/*
 // @run-at   document-start
 // @grant    none
-// @version  1.0
+// @version  2.0
 // @updateURL  https://github.com/SkaneroOo/Tamperki/blob/master/antytestportal.js
 // ==/UserScript==
 
-//window.addEventListener('load', function() {
-//    blurSpy.stop()
-//}, false);
 
-//window.addEventListener("contextmenu", function(e){
-//    e.stopPropagation()
-//}, true);
+window.onerror = () => {};
+document.hasFocus = () => true;
 
-setInterval(function(){
-    blurSpy.stop();
-    e.stopPropagation()
-},1);
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('script.js');
+(document.head||document.documentElement).appendChild(s);
