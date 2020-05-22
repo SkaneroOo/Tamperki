614785e2d988eb9abbaae58e4cad8dc63b647287
SkaneroOo <40175161+SkaneroOo@users.noreply.github.com>
2020-05-22 09:52:24 +0200

    Create antytestportal.js

---
+++ antytestportal.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name     Anty testportal
+// @include  https://www.testportal.pl/*
+// @run-at   document-start
+// @grant    none
+// @version  1.0
+// @updateURL  
+// ==/UserScript==
+
+//window.addEventListener('load', function() {
+//    blurSpy.stop()
+//}, false);
+
+//window.addEventListener("contextmenu", function(e){
+//    e.stopPropagation()
+//}, true);
+
+setInterval(function(){
+    blurSpy.stop();
+    e.stopPropagation()
+},1);
