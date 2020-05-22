a712ca354574387acdfcaf79c474c8f40ed8f1c3
SkaneroOo <40175161+SkaneroOo@users.noreply.github.com>
2020-05-22 10:03:23 +0200

    Update antytestportal.js

--- antytestportal.js
+++ antytestportal.js
@@ -14,3 +14,7 @@ document.hasFocus = () => true;
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('script.js');
 (document.head||document.documentElement).appendChild(s);
+
+window.addEventListener("contextmenu", function(e){
+     e.stopPropagation()
+}, true);
